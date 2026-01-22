83e6fa8c524041532a6fa1539d533b216bdb1e18
Aveion <aveionvang@gmail.com>
2026-01-22 01:31:24 -0500

    Retuning Pedro, finished constants.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -16,16 +16,11 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
             .mass(11.3398)
-            .lateralZeroPowerAcceleration(79.7) //
-            .forwardZeroPowerAcceleration(79.7) // If too much overshoot or aggression when braking lower to 70.
-            //.useSecondaryTranslationalPIDF(true)
-            //.useSecondaryHeadingPIDF(true)
-            //.useSecondaryDrivePIDF(true)
-            .translationalPIDFCoefficients(new PIDFCoefficients(0.16, 0.00000005,0.011,0.026))
-            .headingPIDFCoefficients(new PIDFCoefficients(1.45, 0, 0.08, 0));
+            .lateralZeroPowerAcceleration(-42.66634463218226) //
+            .forwardZeroPowerAcceleration(-84.66708322242962);
 
     public static MecanumConstants driveConstants = new MecanumConstants()
-            .maxPower(0.4)
+            .maxPower(1)
             .rightFrontMotorName("FR")
             .rightRearMotorName("BR")
             .leftRearMotorName("BL")
@@ -34,10 +29,10 @@ public class Constants {
             .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
             .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
             .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
-            .yVelocity(64.1)
-            .xVelocity(51.28); // Because no x pod use 80% of yVelocity
+            .yVelocity(79.87109230822466)
+            .xVelocity(64.44620928426427);
 
-    public static PathConstraints pathConstraints = new PathConstraints(0.8, 40, Math.toRadians(180), 1);
+    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
@@ -49,12 +44,11 @@ public class Constants {
 
     // GO BACK TO PEDRO PATH STEPS AND GO TO ENCODER RESOLUTION.
     public static PinpointConstants localizerConstants = new PinpointConstants()
-            //.forwardPodY(4) // Y-Pod distance to center
-            .forwardPodY(0)
-            .strafePodX(0.625) // X-Pod distance to center
+            .forwardPodY(4) // Y-Pod distance to center
+            .strafePodX(0.75) // X-Pod distance to center
             .distanceUnit(DistanceUnit.INCH)
             .hardwareMapName("pinpoint")
-            .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
-            .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD)
-            .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD);
+            .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_SWINGARM_POD)
+            .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.REVERSED)
+            .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.REVERSED);
 }
